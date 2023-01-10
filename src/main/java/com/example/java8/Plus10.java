9fcce8f66f7696a91ba2605c90d3219a56eefd12
82108 <dudals789@naver.com>
2023-01-10 23:14:03 +0900

    자바에서 제공하는 함수형 인터페이스

---
+++ src/main/java/com/example/java8/Plus10.java
@@ -0,0 +1,54 @@
+package com.example.java8;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Plus10 implements Function<Integer, Integer> {
+
+    @Override
+    public Integer apply(Integer integer) {
+        return integer + 10;
+    }
+
+    public static void main(String[] args){
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(10));
+
+        //
+        Function<Integer, Integer> plus11 = (i) ->{
+            return i+10;
+        };
+
+        System.out.println(plus11.apply(11));
+
+        //
+        Function<Integer, Integer> multiply2 = (i) ->{
+            return i*2;
+        };
+
+        System.out.println(multiply2.apply(1));
+
+        // multiply2 를 하고 plus11
+        Function<Integer, Integer> compose = plus11.compose(multiply2);
+        System.out.println(compose.apply(1));
+
+        // plus11 를 하고 multiply2
+        Function<Integer, Integer> integerIntegerFunction = plus11.andThen(multiply2);
+        System.out.println(integerIntegerFunction.apply(2));
+
+
+        // BIFunction(입력 값을 2개 받아서 1개 반환)
+        // Comsumer(입력 값 1개만 받음)
+
+        Consumer<Integer> c = (i) -> System.out.println(i);
+        c.accept(10);
+
+        // Suppier(반환 값만 있음)
+
+        // Preditable(T 타입을 받아서 boolean 을 리턴)
+
+        // UnaryOperator 입력값과 결과 값의 타입이 같은 경우
+        
+        // BinaryOperaotr 입력값 2개 결과값 1개가 전부 같을 것이라고 봄
+    }
+}
